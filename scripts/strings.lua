67a0b3d3497355e90816152300685d7f419e2e8f
Gamnraa <ltaz1466@gmail.com>
2025-03-22 14:56:33 -0400

    DimondTimbs survivor quotes

--- scripts/strings.lua
+++ scripts/strings.lua
@@ -10,3 +10,161 @@ STRINGS.CHARACTERS.GRAMNESS.ACTIONFAIL.CAST_PSI = {
     TARGET_HAS_BUFF = "I just gave them a helping hand!",
     ALREADY_TARGETTED = "I got them already!"
 }
+
+
+STRINGS.CHARACTERS.WILSON.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Good day, %s!",
+    ATTACKER = "%s should swing his bat somewhere else!",
+    MURDERER = "%s is a psychic murderer!",
+    REVIVER = "%s is quite the young hero!",
+    GHOST = "Looks like %s got their head handed to him…",
+    FIRESTARTER = "I thought it was your friend who knew PK Fire…",
+}
+STRINGS.CHARACTERS.WOLFGANG.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Is tiny psychic child, %s. Hello!",
+    ATTACKER = "%s wants to fight huh? Come on!",
+    MURDERER = "T-tiny psychic is using powers for evil!",
+    REVIVER = "Little Psychic is friend of Wolfgang!",
+    GHOST = "Who killed %s, Wolfgang will smash!",
+    FIRESTARTER = "Wolfgang thought %s would be more careful!",
+}
+STRINGS.CHARACTERS.WAXWELL.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Greetings Mr. %s",
+    ATTACKER = "%s Is doing something devious with his PSI!",
+    MURDERER = "Murderer!, I thought you defeated your evil self!",
+    REVIVER = "%s uses his powers well!",
+    GHOST = "We both know the price of revival!",
+    FIRESTARTER = "That’s no place for a fire, %s!",
+}
+STRINGS.CHARACTERS.WX78.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "WHY DID YOU GO BACK TO BEING A FLESHLING, %s",
+    ATTACKER = "%s‘S EVIL SELF IS COMING BACK, EXCELLENT",
+    MURDERER = "MAYBE I WILL DEVELOP A SECOND ROBOT BODY FOR YOU",
+    REVIVER = "%s IS A USEFUL MINION INDEED",
+    GHOST = "HAH, YOU WERE WEAK ALL ALONG",
+    FIRESTARTER = "YES, USE YOUR POWERS TO BURN AND DESTROY",
+}
+STRINGS.CHARACTERS.WILLOW.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hey %s, tell me more about this \"PK Fire\" stuff!",
+    ATTACKER = "Take it easy kid!",
+    MURDERER = "Murderer! Better hope your bat is fireproof!",
+    REVIVER = "Alright, I'm sorry for burning your bat that one time…",
+    GHOST = "Don’t worry, I’ll go grab a heart.",
+    FIRESTARTER = "HAHA, THAT’S the spirit %s!",
+}
+STRINGS.CHARACTERS.WENDY.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hey, %s!",
+    ATTACKER = "%s Is getting violent…",
+    MURDERER = "Abby and I will put an end to you, %s!",
+    REVIVER = "You’re a good friend %s!",
+    GHOST = "Abigail will keep you company while I get you a heart, %s.",
+    FIRESTARTER = "Let’s play a different game, %s…",
+}
+STRINGS.CHARACTERS.WOODIE.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "%s! Hey little buddy!",
+    ATTACKER = "Whoa, calm down with that bat of yours bud!",
+    MURDERER = "You’re in for it now kid!",
+    REVIVER = "Hey, I owe ya, %s!",
+    GHOST = "Good work little buddy!",
+    FIRESTARTER = "Walter oughta teach you some fire saftey!",
+}
+
+STRINGS.CHARACTERS.WICKERBOTTOM.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Ah, greeting dear %s!",
+    ATTACKER = "Don’t make me confiscate that baseball bat young man!",
+    MURDERER = "Your mother would be ashamed, young man!",
+    REVIVER = "My, how sweet of you dear!",
+    GHOST = "Don’t fret dear, I'll get you sorted!",
+    FIRESTARTER = "It’s not safe to play with fire dearie!",
+}
+STRINGS.CHARACTERS.WEBBER.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hey %s. Wanna play some baseball?!",
+    ATTACKER = "Hey if you don’t wanna play you could’ve just said so %s!",
+    MURDERER = "How could you %s!?",
+    REVIVER = "Thanks %s, you’re a great friend!",
+    GHOST = "Oh no! We’ll get help %s!",
+    FIRESTARTER = "Um, we don’t think you’re supposed to do that…!",
+}
+STRINGS.CHARACTERS.WATHGRITHR.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Blessings upon you, magic child %s",
+    ATTACKER = "You wish tö cross your club with my spear %s?, So be it.",
+    MURDERER = "Have at thee, and yöur wicked magic!",
+    REVIVER = "You were chosen by the gods for good reason.",
+    GHOST = "A heart now for the magical child!",
+    FIRESTARTER = "Ah, be wary of wear you practice new magic young warrior, %s.",
+}
+STRINGS.CHARACTERS.WINONA.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hey kiddo, hit any home runs lately?",
+    ATTACKER = "Yeesh kid, watch where you train your hitting!",
+    MURDERER = "%s just murdered them, get em!",
+    REVIVER = "Thanks, I owe ya, %s!",
+    GHOST = "Count on me, %s!",
+    FIRESTARTER = "Be careful, fires can get out of hand, %s!",
+}
+STRINGS.CHARACTERS.WORTOX.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hmmm, PSI is new!",
+    ATTACKER = "I’ll stop sneaking hot sauce in your burgers, I swear!",
+    MURDERER = "Eep! Please don’t kill me, I'm just a poor little imp!",
+    REVIVER = "%s you have a courageous soul!",
+    GHOST = "Don’t worry, I won’t eat you!",
+    FIRESTARTER = "Hyuhyuhyu what a fun prank!",
+}
+STRINGS.CHARACTERS.WORMWOOD.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hi magic friend %s!",
+    ATTACKER = "Ah why you so hurty, %s?!",
+    MURDERER = "%s did very bad thing!",
+    REVIVER = "You good friend, %s!",
+    GHOST = "%s need help!",
+    FIRESTARTER = "Ah, why you make fire?!",
+}
+STRINGS.CHARACTERS.WARLY.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Salut %s, I’ve been working on a new recipe if you would like to try it!",
+    ATTACKER = "I miss my Mama too, there’s no need to get violent about it!",
+    MURDERER = "Mon dieu! How could you do such a horrible thing!",
+    REVIVER = "Thank you petite monsieur, I’ll make you some burgers later!",
+    GHOST = "I don't think there's any hospitals out here, haha!",
+    FIRESTARTER = "Ah, did willow give you matches, %s?!",
+}
+STRINGS.CHARACTERS.WURT.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hi hat boy!",
+    ATTACKER = "Glurph, thought you were friend.",
+    MURDERER = "Never should’ve trusted you, florp!",
+    REVIVER = "You good friend, Flort!",
+    GHOST = "Don’t worry hat boy, will get you heart!",
+    FIRESTARTER = "Wicker not gonna read story because of you!",
+}
+STRINGS.CHARACTERS.WALTER.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Hey %s, you gotta tell me about some of the things you fought sometime!",
+    ATTACKER = "Hey! That was an accident…right?!",
+    MURDERER = "You went too far!",
+    REVIVER = "Thanks %s, glad to be in your party!",
+    GHOST = "Don’t worry %s, Woby and I are on it!",
+    FIRESTARTER = "Did nobody listen at my fire safety meeting…",
+}
+STRINGS.CHARACTERS.WANDA.DESCRIBE.GRAMNESS =
+{
+    GENERIC = "Oh, %s you should tell me about this \"Phase distorter\" sometime!",
+    ATTACKER = "How strange, most timeless he doesn't do that",
+    MURDERER = "That \"Giygas\" fellow must’ve gotten the better of him in this timeline!",
+    REVIVER = "Thanks for bringing me back, %s!",
+    GHOST = "I’d expect nothing less of you.",
+    FIRESTARTER = "Did he learn PK Fire in this timeline?",
+}
+
+
